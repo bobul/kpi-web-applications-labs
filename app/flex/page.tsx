013ad582ac8164105745e32fb45e65477a4e9c1f
Ihor Bobulych <ihbobuacc@gmail.com>
2023-11-27 21:39:23 +0200

    Finish flex page

--- app/flex/page.tsx
+++ app/flex/page.tsx
@@ -1,8 +1,16 @@
 import { Badge } from "@/components/ui/badge";
-import { GitHubLogoIcon, InfoCircledIcon, LightningBoltIcon } from "@radix-ui/react-icons";
+import {
+  CameraIcon,
+  GitHubLogoIcon,
+  InfoCircledIcon,
+  LightningBoltIcon,
+  PersonIcon,
+  PlayIcon
+} from "@radix-ui/react-icons";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const images = [
   {
@@ -85,18 +93,51 @@ const githubLinks = [
   },
 ];
 
+const videoLinks = [
+  {
+    title: 'WASM Introduction',
+    link: 'https://www.youtube.com/watch?v=RcHER-3gFXI&ab_channel=ChromeforDevelopers',
+    description: 'Learn the basics of WebAssembly and its role in web development.',
+  },
+  {
+    title: 'Next.js Tutorial',
+    link: 'https://www.youtube.com/watch?v=ZVnjOPwW4ZA&ab_channel=ProgrammingwithMosh',
+    description: 'A comprehensive tutorial on building web applications with Next.js.',
+  },
+  {
+    title: 'Remix.js Getting Started',
+    link: 'https://www.youtube.com/watch?v=RBYJTop1e-g&ab_channel=Academind',
+    description: 'Get started with Remix.js and understand its key concepts.',
+  },
+  {
+    title: 'Prisma Tutorial',
+    link: 'https://www.youtube.com/watch?v=CYH04BJzamo&ab_channel=TraversyMedia',
+    description: 'Learn how to use Prisma for database access in your projects.',
+  },
+  {
+    title: 'WebRTC Explained',
+    link: 'https://www.youtube.com/watch?v=WmR9IMUD_CY&ab_channel=Fireship',
+    description: 'An explanation of WebRTC and its applications in real-time communication.',
+  },
+  {
+    title: 'Nest.js Crash Course',
+    link: 'https://www.youtube.com/watch?v=wqhNoDE6pb4&ab_channel=TraversyMedia',
+    description: 'A fast-paced crash course on building APIs with Nest.js.',
+  },
+];
+
 export default function Page() {
   return (
     <main className="h-screen flex flex-col">
       <div className="w-full h-1/4 bg-teal-200 flex justify-around items-center">
         <div className="w-2/3 p-2">
-          <h1 className="font-bold text-3xl text-gray-50">Web Applications Playground</h1>
-          <p className="text-lg text-gray-50">Welcome to Lab! Let's look at some features.</p>
+          <h1 className="font-bold md:text-3xl text-sm text-gray-50">Web Applications Playground</h1>
+          <p className="md:text-lg text-xs text-gray-50">Welcome to Lab! Let's look at some technologies.</p>
         </div>
         <div className="w-48 self-end pb-12 flex">
           <Badge>
             Made by Ihor Bobulych
-            <LightningBoltIcon className="ml-1" />
+            <PersonIcon className="ml-1 md:inline hidden" />
           </Badge>
         </div>
       </div>
@@ -104,34 +145,66 @@ export default function Page() {
         <div className="w-3/4 flex-col h-full">
           <div className="h-3/4 flex">
             <div className="flex flex-col h-full w-1/3">
-              <div className="h-1/2 bg-teal-300 border border-t-0 border-r-0 border-l-0">
+              <div className="h-1/2 bg-teal-300 border border-t-0 border-r-0 border-l-0 overflow-auto">
                 <h1 className="font-bold text-gray-50 md:text-xl text-xs p-2">
-                  Documentations <InfoCircledIcon className="md:inline hidden" />
+                  Docs <InfoCircledIcon className="md:inline hidden" />
                 </h1>
                 {links.map((link) => {
                   return (
-                      <Link href={link.href}>
+                      <Link href={link.href} key={link.title}>
                           <p className="text-teal-600 underline text-sm ml-4 mb-0.5">{link.title}</p>
                       </Link>
                   )
                 })}
               </div>
-              <div className="h-1/2 bg-teal-300">
+              <div className="h-1/2 bg-teal-300 overflow-auto">
                 <h1 className="font-bold text-gray-50 md:text-xl text-xs p-2">
                   Repositories <GitHubLogoIcon className="md:inline hidden" />
                 </h1>
                 {githubLinks.map((githubLink) => {
                   return (
-                    <Link href={githubLink.href}>
+                    <Link href={githubLink.href} key={githubLink.title}>
                       <p className="text-teal-600 underline text-sm ml-4 mb-0.5">{githubLink.title}</p>
                     </Link>
                   )
                 })}
               </div>
             </div>
-            <div className="h-full w-2/3 bg-teal-400">3</div>
+            <div className="h-full w-2/3 bg-teal-400 overflow-auto">
+              <h1 className="p-2 font-bold text-2xl text-gray-50">
+                Video Guides
+              </h1>
+              {videoLinks.map((videoLink) => {
+                return (
+                  <div className="w-full p-2 flex items-center justify-around" key={videoLink.title}>
+                    <div className="w-3/4">
+                      <h2 className="text-gray-50 underline">
+                        {videoLink.title}
+                      </h2>
+                      <p className="text-xs text-gray-50 pl-2 pt-1">
+                        {videoLink.description}
+                      </p>
+                    </div>
+                    <Link href={videoLink.link}>
+                      <Button>
+                        Watch
+                      </Button>
+                    </Link>
+                  </div>
+                )
+              })}
+            </div>
           </div>
-          <div className="w-full h-1/4 bg-teal-200">0</div>
+          <div className="w-full h-1/4 bg-teal-200">
+            <div className="w-[150px] h-[30px] bg-teal-400 ml-4 text-gray-50 md:text-base text-xs text-center border border-l-gray-50 border-r-gray-50 border-b-gray-50 border-t-0 border-solid border-gray-50">
+              Summary
+            </div>
+            <p className="text-teal-800 md:text-base text-xs p-1">
+              In the ever-evolving landscape of web development, embracing innovative technologies is the key to staying
+              at the forefront. The tech stack we've curated opens doors to a world of powerful tools and frameworks
+              that are shaping the future of the web.
+            </p>
+          </div>
         </div>
         <div className="w-1/4 h-full overflow-auto bg-teal-300">
           <h1 className="p-2 text-gray-50 md:text-sm text-xs font-bold">
@@ -189,6 +262,7 @@ export default function Page() {
                 <Image
                   src={image.src}
                   alt={image.alt}
+                  key={image.alt}
                   width={40}
                   height={40}
                   className="md:block hidden"
